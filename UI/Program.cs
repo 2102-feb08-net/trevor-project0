10eeb87954ddef684b5fe15ba753f72e5a6cfc10
Trevor Dunbar <tdunbar123@yahoo.com>
2021-02-22 00:28:19 -0500

    Moved UI to its own project

---
+++ UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using DAL;
+
+namespace UI
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            StoreApp storeapp = new StoreApp();
+            storeapp.Run();
+        }
+    }
+}
